abfae0733f33fb576eb4322b56fc4620e8a31fe8
Fabiano Teixeira <fabianotxss@gmail.com>
2025-07-20 15:29:01 -0300

    asjuste consumer cancelled

--- src/FastTechFoodsOrder.Api/Consumers/OrderCancelledConsumer.cs
+++ src/FastTechFoodsOrder.Api/Consumers/OrderCancelledConsumer.cs
@@ -20,29 +20,37 @@ namespace FastTechFoodsOrder.Api.Consumers
 
         public async Task HandleAsync(OrderCancelledMessage message, Activity? activity = null)
         {
-            using var childActivity = activity?.Source.StartActivity("OrderCancelledConsumer.HandleAsync");
-            childActivity?.SetTag("order.id", message.OrderId);
-            childActivity?.SetTag("message.type", "OrderCancelled");
-
-            _logger.LogInformation("Order CANCELLED - OrderId: {OrderId}, Customer: {CustomerId}, Reason: {CancelReason}",
-                message.OrderId, message.CancelledBy, message.CancelReason);
-
-            var id = message.OrderId;
-            var dto = new UpdateOrderStatusDto
+            try
             {
-                Status = OrderStatusUtils.ConvertStatusToString(Shared.Enums.OrderStatus.Cancelled),
-                UpdatedBy = message.CancelledBy,
-            };
-            var updated = await _orderService.UpdateOrderStatusAsync(id, dto);
+                using var childActivity = activity?.Source.StartActivity("OrderCancelledConsumer.HandleAsync");
+                childActivity?.SetTag("order.id", message.OrderId);
+                childActivity?.SetTag("message.type", "OrderCancelled");
 
-            if (!updated)
-            {
-                _logger.LogError("Failed to update order status to CANCELLED for OrderId: {OrderId}", message.OrderId);
-                childActivity?.SetTag("operation.success", false);
-                throw new Exception($"Failed to update order status to CANCELLED for OrderId: {message.OrderId}");
+                _logger.LogInformation("Order CANCELLED - OrderId: {OrderId}, Customer: {CustomerId}, Reason: {CancelReason}",
+                    message.OrderId, message.CancelledBy, message.CancelReason);
+
+                var id = message.OrderId;
+                var dto = new UpdateOrderStatusDto
+                {
+                    Status = OrderStatusUtils.ConvertStatusToString(Shared.Enums.OrderStatus.Cancelled),
+                    UpdatedBy = message.CancelledBy,
+                };
+                var updated = await _orderService.UpdateOrderStatusDirectAsync(id, dto);
+
+                if (!updated)
+                {
+                    _logger.LogError("Failed to update order status to CANCELLED for OrderId: {OrderId}", message.OrderId);
+                    childActivity?.SetTag("operation.success", false);
+                    throw new Exception($"Failed to update order status to CANCELLED for OrderId: {message.OrderId}");
+                }
+
+                childActivity?.SetTag("operation.success", true);
             }
-
-            childActivity?.SetTag("operation.success", true);
+            catch (Exception ex)
+            {
+                _logger.LogError("Error processing OrderCancelledMessage for OrderId: {OrderId}", message.OrderId);
+                throw ex;
+            }
         }
     }
 }
